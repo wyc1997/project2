e39e46745c68006c8a627d2515d9b7094ec09f0a
wyc1997 <home@wuyangchaodeMacBook-Air.local>
2018-07-30 23:52:10 +0800

    completed most functions but need to fix problem with adding new message to the channel

--- application.py
+++ application.py
@@ -1,15 +1,46 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 from flask_socketio import SocketIO, emit
+from datetime import timedelta
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = timedelta(seconds=1)
 socketio = SocketIO(app)
 
 channels = []
-
+channelContent = {}
+current_channel = ''
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template('index.html')
+
+@socketio.on("connect")
+def initialization():
+    emit("initChannelList", {"existingChannels": channels}, broadcast=True)
+    if current_channel != '':
+        emit("initChannelContent", {"initChannel": channelContent[current_channel]}, broadcast=True)
+
+@socketio.on("submit channel")
+def addChannel(data):
+    newChan = data["newChan"]
+    channels.append(newChan)
+    emit("updateChannelList", {"newChan": newChan}, broadcast=True)
+
+@socketio.on("change channel")
+def ch_channel(data):
+    current_channel = data["current_channel"]
+    emit("initChannelContent", {"initChannel": channelContent[current_channel]}, broadcast=True)
+
+@socketio.on("submit text")
+def addTextToChannel(data):
+    text = data["text"]
+    current_channel = data["current_channel"]
+    current_time = data["current_time"]
+    current_user = data["current_user"]
+    if current_channel not in channelContent.keys():
+        channelContent[current_channel] = []
+    channelContent[current_channel].append({"text": text, "user": current_user, "time": current_time})
+    emit('refresh channel', {"newMessage": {"text": text, "user": current_user, "time": current_time}}, broadcast=True)
--- static/index.js
+++ static/index.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
     if (!localStorage.getItem('username')){
         document.querySelector('#userLine').innerHTML = 'Please input a username';
     }
     else {
         document.querySelector('#userLine').innerHTML = `User: ${localStorage.getItem('username')}`;
+    }
+
+    if (!localStorage.getItem('current_channel')){
+        document.querySelector('#channel_title').innerHTML = 'Please select a channel';
+    }
+    else {
+        document.querySelector('#channel_title').innerHTML = `Channel: ${localStorage.getItem('current_channel')}`;
     }
 
     document.querySelector('#new_users').onsubmit = () => {
@@ -14,20 +24,67 @@ document.addEventListener('DOMContentLoaded', () => {
         return false;
     }
 
-    document.querySelector("#new_channel").onsubmit = () => {
-        const li = document.createElement('li');
-        li.innerHTML = document.querySelector('#input_channel').value;
-        li.className = 'ch';
-        li.setAttribute('onclick', 'changeChannel()')
+
+    socket.on('updateChannelList', data => {
+        let li = document.createElement('li');
+        li.className = "ch";
+        li.onclick = function() {
+            localStorage.setItem('current_channel', this.innerHTML);
+            socket.emit('change channel', {'current_channel': localStorage.getItem('current_channel')})
+            document.querySelector('#channel_title').innerHTML = `Channel: ${localStorage.getItem('current_channel')}`;
+        };
+        li.innerHTML = data.newChan;
         document.querySelector('#channel_list').append(li);
-        document.querySelector('#input_channel').value = '';
-        return false;
-    }
-    
+    })
+
+    socket.on('initChannelList', data => {
+        let exi_channels = data.existingChannels;
+        for (var i = 0; i < exi_channels.length; i++) {
+            const li = document.createElement('li');
+            li.innerHTML = exi_channels[i];
+            li.className = "ch"
+            li.onclick = function() {
+                localStorage.setItem('current_channel', this.innerHTML);
+                socket.emit('change channel', {'current_channel': localStorage.getItem('current_channel')})
+                document.querySelector('#channel_title').innerHTML = `Channel: ${localStorage.getItem('current_channel')}`;
+            };
+            document.querySelector('#channel_list').append(li);
+        }
+    })
+
+    socket.on('initChannelContent', data => {
+        let curChannelData = data.initChannel;
+        for (var i = 0; i < curChannelData.length; i++) {
+            const li = document.createElement("li");
+            li.innerHTML = `${curChannelData[i].text} by ${curChannelData[i].user}, at ${curChannelData[i].time}`;
+            document.querySelector("#messages").append(li);
+        }
+    })
+
+    socket.on('connect', () => {
+        document.querySelector('#text_form').onsubmit = () => {
+            const text = document.querySelector('#text_input').value;
+            var current_time = new Date()
+            socket.emit('submit text', {'text': text, 'current_channel': localStorage.getItem('current_channel'),
+                                        'current_user': localStorage.getItem('username'), 'current_time': current_time})
+            text = '';
+            return false;
+        }
+
+        document.querySelector("#new_channel").onsubmit = () => {
+            socket.emit('submit channel', {"newChan": document.querySelector('#input_channel').value});
+            document.querySelector('#input_channel').value = '';
+            return false;
+        }
+
+    })
+
+    socket.on('refresh channel', data => {
+        const li = document.createElement("li");
+        let message = data.newMessage;
+        li.innerHTML = `${message.text} by ${message.user}, at ${message.time}`;
+        document.querySelector("#messages").append(li);
+    })
+
 
 })
-
-function changeChannel() {
-    alert('you did it');
-    return;
-}
